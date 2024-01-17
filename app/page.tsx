984d81f70e43a64e75e8628b5a31a9a0c3acca1f
JunDemi <jungwook3176@gmail.com>
2024-01-17 16:52:36 +0900

    Feat: 메타데이터 타이틀 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
+  
   return (
+    <>
    <div>
     Hello World
    </div>
+    </>
   )
 }
---
+++ app/search/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: '검색',
+}
+
+export default function Search() {
+  
+  return (
+    <>
+   <div>
+    Hello World
+   </div>
+    </>
+  )
+}
