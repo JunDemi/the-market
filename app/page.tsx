77e75708daa2370f3bd5b5584c13d73e1dbba26c
JunDemi <jungwook3176@gmail.com>
2024-01-19 14:52:06 +0900

    CI: SCSS, Styled Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import NavBar from "@/components/NavBar";
+import "../asset/main.scss";
+
 export default function Home() {
   
   return (
     <>
-   <div>
-    Hello World
+  <NavBar/>
+   <div className="main-dashboard">
+    Hello
    </div>
     </>
   )
---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+import "../asset/main.scss";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+    position: fixed;
+    background-color: red;
+`;
+
+
+export default function NavBar() {
+  
+  return (
+    <>
+    <NavContainer>
+        Hello
+    </NavContainer>
+    </>
+  )
+}
