65eedb19d2cd4d0c92ae03df58051dd9e8c36f12
JunDemi <jungwook3176@gmail.com>
2024-01-27 15:50:23 +0900

    Fix: 로그인 유효성 검사 수정

--- validationSchema/auth.ts
+++ validationSchema/auth.ts
@@ -4,7 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 //로그인 페이지 유효성 검사
 const loginSchema = Yup.object({
     email: Yup.string().email("유효하지 않은 이메일입니다.").required("이메일을 입력하세요."),
-    password: Yup.string().required("비밀번호를 입력하세요.").min(5, "비밀번호는 최소 5글자 이상이어야 합니다.")
+    password: Yup.string().required("비밀번호를 입력하세요.")
 });
 export const LoginValidation = () => useForm({
     resolver: yupResolver(loginSchema),
